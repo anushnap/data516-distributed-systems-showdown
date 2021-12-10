9c009829574b899d45a27488671d839b46d2a607
anushnap <anushnap@uw.edu>
2021-12-09 21:00:46 -0800

    adding other files

--- queries/convert_tsv_pd.py
+++ queries/convert_tsv_pd.py
@@ -3,11 +3,13 @@ import pandas as pd
 from tqdm import tqdm
 
 # tsv_files = ['../data/title.principals.tsv', '../data/title.ratings.tsv', '../data/title_akas.tsv']
-tsv_files = ['../data/title_akas.tsv']
+tsv_files = ['../data/title.ratings.tsv']
 
 for tsv in tsv_files:
+    new_fname = '../data/csvs/' + tsv[8:-4] + '.csv'
     print('Reading dataframe in chunks: ' + tsv)
     dfs = pd.read_csv(tsv, sep='\t', chunksize=50)
+    
     for df in tqdm(dfs):
         # print('Writing dfs')
-        df.to_csv('../data/test_tsv_conversion/file.csv', sep='|', index=False, mode='a')
+        df.to_csv(new_fname, sep='|', index=False, mode='a')
